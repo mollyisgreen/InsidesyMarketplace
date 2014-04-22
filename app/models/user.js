95c60bb75593ea8c0a64e2823982d9c165a6ccd0
Molly Yang <mollyisgreen@gmail.com>
2014-04-22 15:19:07 -0700

    fixed duplicate key problem sign up. now to get sign up legitimacy checks

---
+++ app/models/user.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+var userSchema = mongoose.Schema({
+    local       : {
+    	name		: String,
+        email       : {type: String, unique: true, sparse: true},
+        password    : String,
+        updated_at  : Date 
+    }
+}) 
+ 
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
