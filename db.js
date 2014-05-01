d16fc13ce1b33d69018999bb29b57dbdc6280e6d
Molly Yang <mollyisgreen@gmail.com>
2014-04-30 18:44:15 -0700

    fixing uploading bugs. in proper order but not saving guidepath always

--- db.js
+++ db.js
@@ -98,50 +98,60 @@ var Guide = mongoose.model( 'Guide', guideSchema );
 
 exports.uploadguide = function(req, res){
 
-    // get the temporary location of the file
-    var tmp_path = req.files.guide.path;
-    // set where the file should actually exists - in this case it is in the "images" directory
-    //TODO: create some unique identifier as the file path addendum, not the guide name, 
-            //TODO: which isn't necessarily unique
-    var target_path = './public/guideuploads/' + req.files.guide.name;
-    // move the file from the temporary location to the intended location
-    fs.rename(tmp_path, target_path, function(err) {
-        if (err) throw err;
-        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
-        fs.unlink(tmp_path, function() {
-            if (err) throw err;
-            res.send('File uploaded to: ' + target_path + ' - ' + req.files.guide.size + ' bytes');
-        });
-    });
-
-
     var guide = new Guide({
         // TODO: add parts
-        guidePath    : target_path,
+        //guidePath    : target_path,
         authorEmail : "ejim@gmail.com",
         updated_at : Date.now()   
     });
 
     guide.save(function (err) {
+        console.log("baby1");
         if (!err) {
+            console.log(guide.id);
+            // get the temporary location of the file
+            var tmp_path = req.files.guide.path;
+            // set where the file should actually exists - in this case it is in the "images" directory
+            //var target_path = './public/guideuploads/' + req.files.guide.name;
+            var target_path = './public/guideuploads/' + guide.id;
+            console.log("baby2");
+            // move the file from the temporary location to the intended location
+            fs.rename(tmp_path, target_path, function(err) {
+                if (err) throw err;
+                // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+                fs.unlink(tmp_path, function() {
+                    if (err) throw err;
+                    res.send('File uploaded to: ' + target_path + ' - ' + req.files.guide.size + ' bytes');
+                });
+            });
+
+            // save guide id to user document
+            // save email from session
+            db.collection("users").update(
+                { "local.email": "ejim@gmail.com" },
+                { $push: { guidearray: guide.id } }, 
+                function (err, result) {
+                    if (err) throw err;
+                    console.log("baby3");
+                });
+
+            db.collection("guides").update(
+                { authorEmail : "ejim@gmail.com" },
+                { $set: { guidePath: target_path} }, 
+                function (err, result) {
+                    if (err) throw err;
+                    console.log("baby4;")
+                });
+
+            res.redirect('/guide');
+
             return res.send(guide);
         } else {
             console.log(err);
             return res.send(404, { error: "Guide was not saved." });
         }
+
     });
-
-    // save guide id to user document
-    // save email from session
-    db.collection("users").update(
-        { "local.email": "ejim@gmail.com" },
-        { $push: { guidearray: guide.id } }, 
-        function (err, result) {
-            if (err) throw err;
-        });
-
-    res.redirect('/guide');
-
 
 }
 
